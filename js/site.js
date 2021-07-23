15cf1cbb19a5332c6492cf809167fd2170e47223
Liam Morrison <liammorrison11@gmail.com>
2021-07-23 16:34:18 -0500

    updated site.js to include speed app functionality. updated app and code pages to reflect changes in site.js page. updated socials with correct URLs.

--- js/site.js
+++ js/site.js
@@ -1,4 +1,54 @@
-// Call HelloWorld
-function HelloWorld() {
-    alert("Hello World!");
+// get the values from the Page
+// Controller function to start/initialize program
+function getValues() {
+    // get values from the page
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+
+    // Validating our input to prevent unwanted inputs
+    // parse into integers
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+
+    // if statement to check that start and end values are ints
+    if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+        // call generateNumbers()
+        let numbers = generateNumbers(startValue, endValue);
+
+        // call displayNumbers()
+        displayNumbers(numbers);
+    } else {
+        alert("You must enter a number in the start and end values.")
+    }
+
+}
+
+// generate numbers from startValue to the endValue
+function generateNumbers(sValue, eValue) {
+    
+    let numbers = [];
+
+    // get all numbers from start to end inclusvie
+    for (let i = sValue; i <= eValue; i++){
+        numbers.push(i);
+        console.log(numbers);
+    }
+
+    return numbers;
+}
+
+// display numbers and mark even in bold
+function displayNumbers(numbers) {
+    let templateRows = "";
+    for (let i = 0; i < numbers.length; i++) {
+        let number = numbers[i];
+
+        if (number % 2 == 0) {
+            templateRows += `<tr><td><strong>${number}</strong></td></tr>`;
+        } else {
+            templateRows += `<tr><td>${number}</td></tr>`;
+        }
+    }
+
+    document.getElementById("results").innerHTML= templateRows;
 }
